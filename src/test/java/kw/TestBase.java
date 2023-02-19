54d0bc5338ef2c26fb88614c8dd79b76b475b862
Iuliia Kuznetcova <iuliia_kuznetcova@hotmail.com>
2023-02-19 15:37:53 +0100

    added public void setupTest(Method m, Object[] p) and Arrays.asList(p) to the lesson from 09.02.23

--- src/test/java/kw/TestBase.java
+++ src/test/java/kw/TestBase.java
@@ -12,27 +12,28 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
-
     WebDriver driver;
     final static Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeClass
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
-        logger.info("Setup chrome driver");
+        logger.info("Setup chrome driver Kuz");
     }
 
     @BeforeMethod
-    public void setupTest() {
+    public void setupTest(Method m, Object[] p) {
         driver = new ChromeDriver();
         driver.get("http://phonebook.telran-edu.de:8080/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         // logger.info("Running a test: prepare");
-        logger.info("Start test");
+        logger.info("Start test Kuz " + m.getName() + " with data: " + Arrays.asList(p));
     }
 
     public void fillField(String userData, By cssSelector) {
@@ -68,11 +69,12 @@ public class TestBase {
     }
 
     @AfterMethod
-    public void tearDown() throws InterruptedException {
+    public void tearDown(Method m) throws InterruptedException {
         Thread.sleep(1000);
         if (driver != null) {
             driver.quit();
         }
-        logger.info("Stop test");
+        logger.info("Stop test Kuz " + m.getName());
+        logger.info("==========================================================================");
     }
 }
